4b54a8d806c4fce23299e708ee2b1dd7130f0507
Tristan Loutan <tll1477@g.rit.edu>
2023-02-25 13:34:47 -0500

    Button has been added

--- PycharmProjects/ServiceSkipper/skipGUI.py
+++ PycharmProjects/ServiceSkipper/skipGUI.py
@@ -1,18 +1,28 @@
 """
 GUI for the skipper
 """
+from tkinter import PhotoImage
+
+import tkinter
 import customtkinter
 from customtkinter import *
+from PIL import Image
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("dark-blue")
 
 # creating the window and setting the size
-gui = CTk()
+gui = customtkinter.CTk()
 gui.geometry("800x600")
 
 # creates a text label
 label = customtkinter.CTkLabel(gui, text="Service Skipper")
 label.pack()
 
+my_image = customtkinter.CTkImage(light_image=Image.open("/Users/trislout/PycharmProjects/ServiceSkipper/myCoursesLogo.png"),
+                                  dark_image=Image.open("/Users/trislout/PycharmProjects/ServiceSkipper/myCoursesLogo.png"),
+                                  size=(30,30))
+
+myCoursesButton = customtkinter.CTkButton(master=gui, image=my_image)
+myCoursesButton.pack(pady=12, padx=12)
 # runs the application
 gui.mainloop()
