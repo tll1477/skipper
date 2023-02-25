1251b36b131db26bc8a2bbc2169ec366dbc725f3
Tristan Loutan <tll1477@g.rit.edu>
2023-02-25 12:52:19 -0500

    Initial window created

--- PycharmProjects/ServiceSkipper/skipGUI.py
+++ PycharmProjects/ServiceSkipper/skipGUI.py
@@ -1,4 +1,16 @@
 """
 GUI for the skipper
 """
+import customtkinter
+from customtkinter import *
 
+# creating the window and setting the size
+gui = CTk()
+gui.geometry("800x600")
+
+# creates a text label
+label = customtkinter.CTkLabel(gui, text="Service Skipper")
+label.pack()
+
+# runs the application
+gui.mainloop()
